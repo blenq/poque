db1f45e3c3e5330015a530490ba3b9b68e16cf72
Bart Lengkeek <bart@lengkeek.com>
2018-02-19 15:24:42 +0100

    Add cursor tests

---
+++ test/test_cursor.py
@@ -0,0 +1,17 @@
+# import unittest
+#
+# from test.config import BaseExtensionTest, BaseCTypesTest
+#
+#
+# class CursorTest():
+#     pass
+#
+#
+# class ResultTestValuesExtension(
+#         BaseExtensionTest, CursorTest, unittest.TestCase):
+#     pass
+#
+#
+# class ResultTestValuesCtypes(
+#         BaseCTypesTest, CursorTest, unittest.TestCase):
+#     pass
